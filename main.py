60a8b9b163ff1ec89816d0f28cbc65d809805462
Masstran <15689794+Masstran@users.noreply.github.com>
2021-09-09 17:00:36 +0000

    Combine decrypt and encrypt into single function

--- main.py
+++ main.py
@@ -4,31 +4,22 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text: str, shift: int) -> str:
-  encrypted_text = ""
+def caesar(text: str, shift: int, direction: str) -> str:
+  multiplier = 0
+  if direction == "encode":
+    multiplier = 1
+  elif direction == "decode":
+    multiplier = -1
+  else:
+    print("No idea, what you are doing, I'm just gonna give you your text back")
+  new_text = ""
   for char in text:
     if char not in alphabet:
-      encrypted_text += char
+      new_text += char
       continue
     char_position = alphabet.index(char)
-    encrypted_char_position = (char_position + shift) % len(alphabet)
-    encrypted_text += alphabet[encrypted_char_position]
-  return encrypted_text
+    new_char_position = (char_position + shift * multiplier) % len(alphabet)
+    new_text += alphabet[new_char_position]
+  return new_text
 
-def decrypt(text: str, shift: int) -> str:
-  decrypted_text = ""
-  for char in text:
-    if char not in alphabet:
-      decrypted_text += char
-      continue
-    char_position = alphabet.index(char)
-    decrypted_char_position = (char_position - shift) % len(alphabet)
-    decrypted_text += alphabet[decrypted_char_position]
-  return decrypted_text
-
-if direction == "encode":
-  print(f"The encoded text is: {encrypt(text, shift)}")
-elif direction == "decode":
-  print(f"The decoded text is: {decrypt(text, shift)}")
-else:
-  print("I have no idea what you are trying to do.")
+print(f"Result text is: {caesar(text, shift, direction)}")
